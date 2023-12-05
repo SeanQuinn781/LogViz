9a254ca468695426174bf9eca6fd06e2289833cb
Sean Quinn <SeanQuinn781@gmail.com>
2023-12-05 16:06:57 -0500

    refactoring backend

--- app/t.py
+++ app/t.py
@@ -3,10 +3,40 @@ import re
 from ip2geotools.databases.noncommercial import DbIpCity
 import requests
 
+def geolocate(ip_address):
+    try:
+        url = f"https://geolocation-db.com/jsonp/{ip_address}&position=true"
+        response = requests.get(url)
+        # Clean the returned JSONP response to be a valid JSON
+        json_response = response.text.split("(")[1].strip(")")
+        data = eval(json_response)
+        return {
+            'IP': ip_address,
+            'Country': data.get('country_name', "N/A"),
+            'State': data.get('state', "N/A"),
+            'City': data.get('city', "N/A"),
+            'Latitude': data.get('latitude', "N/A"),
+            'Longitude': data.get('longitude', "N/A")
+        }
+    except Exception as e:
+        # return f"Error for IP {ip_address}: {e}"
+        print(f"Error for IP {ip_address}: {e}")
+        print('trying second API')
+        loc = DbIpCity.get(ip_address, api_key='free')
+        if loc is not None:
+            # print('loc: ', loc)
+            return {
+                'IP': ip_address,
+                'Country': loc.country,
+                'State': loc.region,
+                'City': loc.city,
+                'Latitude': loc.latitude,
+                'Longitude': loc.longitude
+            }
 
 class NginxLogParser:
     def __init__(self):
-        self.data = []  # Store IP and OS
+        self.data = {}  # Store IP and OS
         self.all_ips = {}
 
     def getIP(self, line):
@@ -38,14 +68,13 @@ class NginxLogParser:
 
 
     def parseFile(self, filePath):
-        #  print('filePath is, ', filePath)
         with open(filePath, 'r') as file:
             for line in file:
                 ip = self.getIP(line)
-                if ip and ip not in self.all_ips:
-                    self.all_ips[ip] = ip
+                if ip and ip not in self.data:
                     os = self.getOS(line)
-                    self.data.append({'ip': ip, 'os' : os})
+                    geolocation = geolocate(ip)
+                    self.data[ip] = {'os': os, 'geolocation': geolocation}
 
 # Example usage
 parser = NginxLogParser()
@@ -72,44 +101,14 @@ if os.path.exists(subdir_path) and os.path.isdir(subdir_path):
 else:
     print(f"The directory {subdir} does not exist")
 
-def geolocate(ip_address):
-    try:
-        url = f"https://geolocation-db.com/jsonp/{ip_address}&position=true"
-        response = requests.get(url)
-        # Clean the returned JSONP response to be a valid JSON
-        json_response = response.text.split("(")[1].strip(")")
-        data = eval(json_response)
-        return {
-            'IP': ip_address,
-            'Country': data.get('country_name', "N/A"),
-            'State': data.get('state', "N/A"),
-            'City': data.get('city', "N/A"),
-            'Latitude': data.get('latitude', "N/A"),
-            'Longitude': data.get('longitude', "N/A")
-        }
-    except Exception as e:
-        # return f"Error for IP {ip_address}: {e}"
-        print(f"Error for IP {ip_address}: {e}")
-        print('trying second API')
-        loc = DbIpCity.get(ip_address, api_key='free')
-        if loc is not None:
-            # print('loc: ', loc)
-            return {
-                'IP': ip_address,
-                'Country': loc.country,
-                'State': loc.region,
-                'City': loc.city,
-                'Latitude': loc.latitude,
-                'Longitude': loc.longitude
-            }
+
     
 
-for index, request in enumerate(parser.data):
-    print(index, request)
+# Print parsed data
+for ip, data in parser.data.items():
+    print(f"IP: {ip}, OS: {data['os']}, Geolocation: {data['geolocation']}")
 
-
-print(parser.data)  # Prints the dictionary containing IP addresses and their corresponding OSes.
-
+# This will print each IP address with its OS and geolocation data
 
 
 
